b3e8224f0f1b1991986bdec059c5c7b99a50bc3a
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-06-02 12:39:46 -0700

    Generacion de archivos base para modulo de Odoo

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name":"Nombre del Modulo",
+    "author":"El autor del modulo",
+    "website":"el website del modulo o del autor",
+    "summary":"el resumen del modulo",
+    "data":[
+        'security/ir.model.access.csv'
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}

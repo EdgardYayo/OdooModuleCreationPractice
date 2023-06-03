689b84ab6a438f8ab34841106ff213a974f5d1c2
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-06-02 17:17:48 -0700

    Añadiendo chatter y funcion onchange

--- __manifest__.py
+++ __manifest__.py
@@ -3,6 +3,7 @@
     "author":"El autor del modulo",
     "website":"el website del modulo o del autor",
     "summary":"el resumen del modulo",
+    "depends":["mail"],
     "data":[
         'security/ir.model.access.csv'
         'views/menu.xml',
--- models/patient.py
+++ models/patient.py
@@ -2,11 +2,18 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _inherit = "mail.thread"
     _description = "Patient Records"
 
-    name = fields.Char(string="Patient Name", required=True)
-    age = fields.Integer(string="Age")
-    is_child = fields.Boolean(string="Is Child ?")
+    name = fields.Char(string="Patient Name", required=True, tracking=True)
+    age = fields.Integer(string="Age", tracking=True)
+    is_child = fields.Boolean(string="Is Child ?", tracking=True)
     notes = fields.Text(string="Notes")
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender")
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string="Gender", tracking=True)
     
+    @api.onchange('age')
+    def _onchange_age(self):
+        if self.age <= 10:
+            self.is_child = True
+        else:
+            self.is_child = False
